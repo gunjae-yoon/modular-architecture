40faf4b1e913c709e5832ef842eed4252760e68c
Gunjae Yoon <gjyoon@plaif.com>
2024-11-20 10:24:02 +0900

    Remove instance creation method from Data

--- include/march/base/data.h
+++ include/march/base/data.h
@@ -1,13 +1,13 @@
 #ifndef __march_base_data_h__
 #define __march_base_data_h__
 
+#include <memory>
+
 namespace march {
 	class Data {
 	public:
 		Data() {}
 		virtual ~Data() {}
-		
-		virtual Data* create_instance() = 0;
 	};
 }
 
